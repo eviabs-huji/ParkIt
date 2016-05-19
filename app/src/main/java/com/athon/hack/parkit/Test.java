eb58046ea7b2f886130cfd3819c9923a7f470871
Michal <Aa123456>
2016-05-19 16:50:38 +0300

    vccvcv v

--- app/src/main/java/com/athon/hack/parkit/Test.java
+++ app/src/main/java/com/athon/hack/parkit/Test.java
@@ -9,5 +9,7 @@ public class Test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        int a = 6;
+        a++;
     }
 }
